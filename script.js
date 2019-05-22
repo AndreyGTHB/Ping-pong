2fd7c632ffa83ba32324fe775e2bd5247a19ac02
AndreyGTHB <andr.g.tuk@gmail.com>
2019-05-22 21:27:15 +0700

    Animation added

--- script.js
+++ script.js
@@ -14,9 +14,11 @@ function arc(context, x, y) {
 }
 
 function gameDraw() {
-    canvas.width = canvas.width;
+    ctx.clearRect(0, 0, 400, 400);
+    ctx.lineWidth = 2;
+    ctx.strokeRect(0, 0, 400, 400);
 
-    arc(ctx, x, y)
+    arc(ctx, arcX, arcY)
 }
 
 function paint() {
@@ -29,6 +31,15 @@ function paint() {
 }
 
 
-x = 200;
-y = 20;
+arcX = 200;
+arcY = 20;
 paint();
+
+setInterval(function(){
+    if (arcX >= 390) {
+        arcX = 10;
+    }
+    arcX++;
+}, 7);
+
+
